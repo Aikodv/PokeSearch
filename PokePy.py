08875337179c547f73feba2ed7403bce14e6730f
Aikodv <112638686+Aikodv@users.noreply.github.com>
2022-10-23 17:14:47 -0300

    Django

--- PokePy.py
+++
@@ -1,6 +0,0 @@
-from urllib import response
-import requests
-import json
-url  = "https://pokeapi.co/api/v2/pokemon"
-response = requests.get(url)
-print(response.json)
---
+++ PokeSearch/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+import datetime 
+def prueba(request):
+    return HttpResponse("Hola xd")
+
+def prueba2 (request):
+    return HttpResponse("SExoooooo")
+
+def fecha(request):
+    
+    fecha_actual = datetime.datetime.now()
+    
+print(datetime.datetime.now())
