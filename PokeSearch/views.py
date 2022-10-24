c0435b12587a406f3de15e843be81edfaabef4d7
Aikodv <alvaro.alcaino@usm.cl>
2022-10-24 12:28:27 -0300

    Update views.py

--- PokeSearch/views.py
+++ PokeSearch/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import datetime 
 def prueba(request):
-    return HttpResponse("Hola xd")
+    return HttpResponse("<html><head><title>Esta es mi primera pagina</title></head><body><h1>Esto es un encabezado</h1><p>Y esto es un parrafo, donde podemos escribir todo el rollo que se nos ocurra.</body></html>")
 
 def prueba2 (request):
     return HttpResponse("a")
