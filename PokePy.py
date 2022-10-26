4a731c93ab0c152d046671fa96c061a1e2f20bb9
Aikodv <alvaro.alcaino@usm.cl>
2022-10-26 16:19:15 -0300

    aaaa

--- PokePy.py
+++ PokePy.py
@@ -1,6 +1,6 @@
 from urllib import response
 import requests
 import json 
-url =  "https://pokeapi.co/api/v2/"
+url =  "https://pokeapi.co/api/v2/pokemon/ditto"
 response  = requests.get(url)
 print(response.json())
