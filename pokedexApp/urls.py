2e205d274a30a7e270a75f5902ffc999717a7fee
Aikodv <alvaro.alcaino@usm.cl>
2022-11-19 15:54:57 -0300

    si

--- pokedexApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pokeindex, name='pokeindex'),
-    path("pokedex/", views.index, name="index"),
-    path("home/",views.home, name="home"),
-    path("about_us", views.about_us, name= "about_us")
-]
