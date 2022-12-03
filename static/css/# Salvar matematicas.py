379ceacd2ff27cec0515eccc4ffb4991fc877509
Aikodv <alvaro.alcaino@usm.cl>
2022-12-03 16:16:07 -0300

    sis

--- static/css/# Salvar matematicas.py
+++ static/css/# Salvar matematicas.py
@@ -1,45 +1,48 @@
 # OPERACION SALVAR TODOS LOS RAMOS 
 
- # Operacion Salvar matematicas 
+# Operacion Salvar matematicas 
 
 c1  = 46 # 20%
 c2  = 36 # 25%
-c3  = 70 # 30%
+c3  = 83 # 30%
 co1 = 15
 co2 = 20 
-co3 = 35
+co3 = 56
 promedio_co = (co1+co2+co3)/3
-go = 69
-eta = ((85*0.6+ 100*0.1 +95 *0.3)/450)+(79/90)
+eta = ((((85+98)/2)*0.6+ 100*0.1 +(95+65)/2 *0.3)/450)+(79/90)
 print(eta)
 
-ponderacion = ((promedio_co * 0.25)+ c1*0.20 + c2*0.25 + c3*0.30)*eta
-print("Promedio Matematicas", ponderacion)
-print("Promedio Matematicas con global", ponderacion*0.70+go*0.30)
-
-# Oparicion Salvar quimica
-
-c1  = 33 # 20%
-c2  = 33 # 20%
-c3  = 40 # 25%
-co = (75+80+80+100+100)/5 #20
-Ag = (94+92+100+100+100+100)/6 #15
-promedio_qui = co*0.2 + Ag*0.15 + c1*0.2 + c2*0.2 + c3*0.25
-print("promedio de quimica",promedio_qui)
+ponderacion = ((promedio_co * 0.25)+ c1*0.20 + c2*0.25 + c3*0.30)
+print("Promedio Matematicas sin eta si me saco un 83", ponderacion)
+ponderacion_eta = ponderacion*eta
+print("Promedio Matematicas con eta si me saco un 83 con lab", ponderacion_eta)
+ponderacion = ((promedio_co * 0.25)+ c1*0.20 + c2*0.25 + c3*0.60)
 
 # Operacion Salvar fisica
 
 ce1  = 42 
 ce2  = 57 
-ce3  = 60
-coG  = 80
+ce3  = 52
+coG  = 50
 promedio_certamenes = (ce1+ce2+ce3)/3 #75%
-Controles_fis = (70+80+60)/3
-tareas_fis =(100+90+90)/3 
-promedio_fis = (tareas_fis+Controles_fis)/2
+Controles_fis = (70+80+90+50+30)/5
+tareas_fis =(100+90+95+100+100)/5
+promedio_fis = ((tareas_fis*0.3333)+(Controles_fis*0.6666))
+nose = (promedio_fis*0.25+promedio_certamenes*0.75)
+print("promedio fisica sin eta",nose)
+noseeta = nose*1.03
+print("promedio fisica con eta",noseeta)
+global_fis = (noseeta*0.6+coG*0.4)
+print("promedio fisica con global si me saco un 50",global_fis)
 
-print("promedio fisica",(((promedio_fis*0.25+promedio_certamenes*0.75)*1.05))*0.6+coG*0.4)
+# Operacion Salvar quimica
 
-# aproximate pi with taylor polinomials
-# pi = 4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + 4/13 - 4/15 + 4/17 - 4/19 + 4/21 - 4/23 + 4/25 - 4/27 + 4/29 - 4/31 + 4/33 - 4/35 + 4/37 - 4/39 + 4/41 - 4/43 + 4/45 - 4/47 + 4/49 - 4/51 + 4/53 - 4/55 + 4/57 - 4/59 + 4/61 - 4/63 + 4/65 - 4/67 + 4/69 - 4/71 + 4/73 - 4/75 + 4/77 - 4/79 + 4/81 - 4/83 + 4/85 - 4/87 + 4/89 - 4/91 + 4/93 - 4/95 + 4/97 - 4/99 + 4/101 - 4/103 + 4/105 - 4/107 + 4/109 - 4/111 + 4/113 - 4/115 + 4/117 - 4/119 + 4/121 - 4/123 + 4/125 - 4/127 + 4/129 - 4/131 + 4/133 - 4/135 + 4/137 - 4/139 + 4/141 - 4/143 + 4/145 - 4/147 + 4/149 - 4/151 + 4/153 - 4/155 + 4/157 - 4/159 + 4/161 - 4/163 + 4/165 - 4/167 + 4/169 - 4/171 + 4/173 - 4/175 + 4/177 - 4/179 + 4/181 - 4/183 + 4/185 - 4/187 + 4/189 - 4/191 + 4/193 - 4/195 + 4/197 - 4/
-
+c1  = 33 # 20%
+c2  = 33 # 20%
+c3  = 33 # 25%
+co = (75+80+80+100+100+60+80)/8 #20
+Ag = (94+92+100+100+100+100)/6#15
+promedio_qui = co*0.2 + Ag*0.15 + c1*0.2 + c2*0.2 + c3*0.25 #60
+glob = 60 # 40%
+print("promedio de quimica",promedio_qui)
+print("Promedio de quimica con global (nota:60)",promedio_qui*0.6+glob*0.4)
